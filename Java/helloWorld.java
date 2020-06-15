7530a416da884091e599e705809748f7ef964b09
Kanisque <kanisquemeena@gmail.com>
2020-06-15 23:37:00 +0530

    Starting Java

---
+++ Java/helloWorld.java
@@ -0,0 +1,12 @@
+package Java;
+
+/**
+ * helloWorld
+ */
+public class helloWorld {
+
+    public static void main(String[] args) {
+        String x = "Hello World!";
+        System.out.println(x);
+    }
+}
